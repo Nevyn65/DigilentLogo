3f55b92a530e6fa20471bff72be45d50f970579b
Nevyn65 <846465235@qq.com>
2020-01-16 14:31:19 +0800

    Add files via upload

---
+++ DigilentLogo.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n");
+    printf("\n");
+    printf("      Digilent, INC.\n");
+    printf("\n");
+    printf("              /@@@\n");
+    printf("             /@@@@@\n");
+    printf("            /@@//\@@\n");
+    printf("           /@@//  \@@\n");
+    printf("          /@@//    \@@\n");
+    printf("         /@@//      \@@\n");
+    printf("        /@@//________\@@\n");
+    printf("       /@@//@@@@@@@@@@@@@\n");
+    printf("      /@@//@@@@@@@@@@@@@@@\n");
+    printf("\n");
+    printf("\n");
+    printf("     Welcome to Digilent!\n");
+
+
+}
